3cbcf76b94faf051640498853f75f38958f70c72
Yuri <Jolycougar@gmail.com>
2024-08-12 20:16:18 +0300

    add models

--- games/models.py
+++ games/models.py
@@ -1,3 +1,34 @@
+from datetime import date
+
 from django.db import models
 
-# Create your models here.
+
+class GenresModel(models.Model):
+    name = models.CharField("name", max_length=30)
+    description = models.TextField("Description")
+    url = models.SlugField(max_length=160, unique=True)
+
+
+class GamePlatformModel(models.Model):
+    name = models.CharField("name", max_length=30)
+
+
+class DeveloperModel(models.Model):
+    name = models.CharField("name", max_length=30)
+    description = models.TextField("Description")
+
+
+class PublisherModel(models.Model):
+    name = models.CharField("name", max_length=30)
+    description = models.TextField("Descriptions")
+
+
+class GameModel(models.Model):
+    name = models.CharField("name", max_length=100)
+    description = models.TextField("Description")
+    poster = models.ImageField("Poster", upload_to="games/")
+    release = models.DateTimeField("Release game", default=date.today)
+    game_platform = models.ForeignKey(GamePlatformModel, on_delete=models.CASCADE)
+    url = models.SlugField(max_length=130, unique=True)
+    publisher = models.ManyToManyField(PublisherModel)
+    developer = models.ManyToManyField(DeveloperModel)
