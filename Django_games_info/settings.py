b6ac51945ccab62e530f882e4db7f5557756eef4
Yuri <Jolycougar@gmail.com>
2024-09-12 20:20:25 +0300

    Add lang

--- Django_games_info/settings.py
+++ Django_games_info/settings.py
@@ -132,6 +132,10 @@ LANGUAGES = (
     ('en', gettext('English')),
 )
 
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
