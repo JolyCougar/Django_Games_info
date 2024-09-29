70ace7dda7f24768e8c984d253c5ccceb26ca6f8
Yuri <Jolycougar@gmail.com>
2024-09-29 21:42:52 +0300

    edit project for docker

--- Django_games_info/settings.py
+++ Django_games_info/settings.py
@@ -188,12 +188,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
 
-MEDIA_URL = ''
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = ''
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 SITE_ID = 1
 
