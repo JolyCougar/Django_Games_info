f9b5389c2086fee4588ebe8bbbdebb45de008fe3
Yuri <Jolycougar@gmail.com>
2024-09-29 16:16:56 +0300

    edit project for docker

--- Django_games_info/settings.py
+++ Django_games_info/settings.py
@@ -27,15 +27,7 @@ DEBUG = bool(int(os.environ.get('DEBUG', default=1)))
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:8081",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:1313",
-    "http://127.0.0.1:1337",
-    "http://localhost:1313",
-    "http://localhost:1337",
-]
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(' ')
 # Application definition
 
 INSTALLED_APPS = [
